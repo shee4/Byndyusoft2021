bd30e039a708535dbac7931a57e9d07d7b2fdbd8
Nik <doc.witcher-377@yandex.ru>
2021-05-14 16:56:55 +0500

    function demonstration in console

--- ByndyusoftBackendTask/Program.cs
+++ ByndyusoftBackendTask/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace ByndyusoftBackendTask
+﻿using System;
+
+namespace ByndyusoftBackendTask
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            var array = new[] {4, 0, 3, 19, 492, -10, 1};
+            var scanner = new SmallestPairScanner();
+            var smallestPair = new SumOfSmallestPairContainer(scanner, array);
+
+            Console.WriteLine("Array of numbers:");
+            foreach (var el in array) 
+                Console.Write(el + " ");
+            Console.Write("\nSmallest pair:" + smallestPair.SmallestPair + "\nSum of smallest pair: ");
+            Console.Write(smallestPair.Sum);
         }
     }
 }
