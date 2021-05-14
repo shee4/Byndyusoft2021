e9086de038ba0f7cd8232cdd9f9dc2a66364564d
Nik <doc.witcher-377@yandex.ru>
2021-05-14 16:08:00 +0500

    Add class contains array and it's smallest pair.

---
+++ ByndyusoftBackendTask.Tests/SumOfSmallestPairContainerTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace ByndyusoftBackendTask.Tests
+{
+    [TestFixture]
+    public class SumOfSmallestPairContainerTests
+    {
+        [Test]
+        public void GetSumFromContainer_ArrayWithMoreThanOneElement_ExpectedSum()
+        {
+            var firstCase = new[] {2, 3};
+            var secondCase = new[]{4, 0, 3, 19, 492, -10, 1};
+            var thirdCase = new int[100000000];
+
+            var scanner = new SmallestPairScanner();
+
+            var firstContainer = new SumOfSmallestPairContainer(scanner, firstCase);
+            var firstCaseResult = firstContainer.Sum;
+
+            var secondContainer = new SumOfSmallestPairContainer(scanner, secondCase);
+            var secondCaseResult = secondContainer.Sum;
+
+            var thirdContainer = new SumOfSmallestPairContainer(scanner, thirdCase);
+            var thirdCaseResult = thirdContainer.Sum;
+
+            Assert.AreEqual(5, firstCaseResult);
+            Assert.AreEqual(-10, secondCaseResult);
+            Assert.AreEqual(0, thirdCaseResult);
+        }
+    }
+}
---
+++ ByndyusoftBackendTask/SumOfSmallestPairContainer.cs
@@ -0,0 +1,17 @@
+﻿namespace ByndyusoftBackendTask
+{
+    public class SumOfSmallestPairContainer
+    {
+        public (int, int) SmallestPair { get; }
+        
+        public int Sum => SmallestPair.Item1 + SmallestPair.Item2;
+
+        public int[] Array { get; }
+
+        public SumOfSmallestPairContainer(IScanner scanner, int[] array)
+        {
+            Array = array;
+            SmallestPair = scanner.GetSmallestPair(array);
+        }
+    }
+}
